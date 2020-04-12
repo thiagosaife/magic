c8d1b3be27c66cc699386f5b4af3b861f6ec219f
Thiago Saife Rodrigues <thiago.rodrigues@THIAGORODRIGUES.local>
2020-04-12 14:15:35 -0300

    Corrigindo link do MTG Wiki.

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,7 @@
     <h3>Magic Cards Info</h3>
     <small>マジキカルドインフ</small>
     <p class="about pt-3">
-      Infos, stats and sprites: <a href="https://mtg.gamepedia.com/" target="_blank">MTG Wiki</a>
+      Infos, stats and sprites: <a href="https://mtg.gamepedia.com/Magic:_The_Gathering/Statistics_and_trivia" target="_blank">MTG Wiki</a>
     </p>
     <p>
       API: <a href="https://docs.magicthegathering.io/" target="_blank">MTG Developers</a>
